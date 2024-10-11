bafcdb84d7462bcb49c0af31cea1f3812b35810e
tkql0 <96943505+tkql0@users.noreply.github.com>
2024-10-11 17:46:52 +0900

    (Update)Scene
    (Update)Scene
     - 타일맵 수정
     - 개인과제를 위한 UI 추가

--- Assets/2.Scripts/SubUI.cs
+++ Assets/2.Scripts/SubUI.cs
@@ -1,18 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class SubUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void CharacterSelect()
     {
         
     }
-
-    // Update is called once per frame
-    void Update()
+    public void NameInput()
     {
-        
+
     }
 }
