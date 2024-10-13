589b7c5ba00b6d4f899aaec63e12a48a3ba798b7
tkql0 <96943505+tkql0@users.noreply.github.com>
2024-10-14 04:14:59 +0900

    (Feat)MainUI
    (Feat)MainUI
     - 시간 추가

--- Assets/2.Scripts/MainUI.cs
+++ Assets/2.Scripts/MainUI.cs
@@ -2,8 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using System;
 
 public class MainUI : MonoBehaviour
 {
-    public TMP_Text text;
+    [SerializeField]
+    private TMP_Text timeText;
+
+    private void Update()
+    {
+        timeText.text = DateTime.Now.ToString("HH : mm");
+    }
+
+    public void ButtonClick()
+    {
+        GameManager.UI.SetGameStop();
+    }
 }
